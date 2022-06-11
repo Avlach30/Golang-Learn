10a8364200ba19b2175ffdf91b1f66dd7d19600d
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-06-11 08:29:36 +0800

    write another useful information for initialize go app directory before develop

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ func main() {
 	fmt.Println("hello, i'm learning golang"); //* for print data to console/terminal
 }
 
-
+//! Before develop an app with golang, make sure initialize app directory firstly with command 'go mod init 'name-app''
 //* Use command 'go run fileName.go' for running go program
 //* Use command 'go build' for build a executable (.exe) go progam
 	//* Name of builded program depend of module name has initialized before
