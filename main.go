6a08298f0ecc3f11012513c28c8764c9f74ed6e3
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-06-11 08:26:50 +0800

    develop first app in golang and write useful information for base technique

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("hello, i'm learning golang"); //* for print data to console/terminal
+}
+
+
+//* Use command 'go run fileName.go' for running go program
+//* Use command 'go build' for build a executable (.exe) go progam
+	//* Name of builded program depend of module name has initialized before
+	//* Type executable file in terminal for running an builded go program
