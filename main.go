32a805e7374528eeed866f5e41ffb37738e83ce7
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-06-11 13:12:09 +0800

    write requirement todo for basic quiz, contains variable, conditional, and looping

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+func main() {
+	//* TODO
+	/*
+		* 1. Develop a looping program with limitation of string length, with condition if index is even number, print at console
+		* 2. Develop a same looping program with same limitation, but different condition if letter contains vocals only (a, i, u, e, o) 
+	*/
+
+	title := "Golang the best language"
+}
