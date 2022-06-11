1fa0348a61e4d3c60f6bfa39183339b82d139075
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-06-11 20:10:54 +0800

    define function which is return an idex of array element and called it to main function

--- main.go
+++ main.go
@@ -8,4 +8,19 @@ import (
 func main() {
 	tropicalFruits := slicefruits.TropicalFruits();
 	fmt.Println(tropicalFruits)
+
+	itemTarget := "Lychee"
+	index := findIndexArrayOfStrings(itemTarget, tropicalFruits)
+	println(index)
+}
+
+//* Define function whis is return an index of array element
+func findIndexArrayOfStrings(element string, arrSource []string) int {
+	for index, item := range arrSource {
+		if (element == item) {
+			return index
+		}
+	}
+
+	return -1
 }
