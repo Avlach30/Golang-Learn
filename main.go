4d8ae1b44079737581e82d3d699628a42b6c8929
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-06-11 10:55:33 +0800

    define main package with imported calculation package

---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"golang-basic-fundamental/calculation"
+)
+
+func main() {
+	fmt.Println("Base-technique-quiz")
+	
+	result := calculation.Multiply(23, 7)
+	fmt.Println(result)
+}
