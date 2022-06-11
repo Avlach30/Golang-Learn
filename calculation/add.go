e1f44fe5f3b41f456b9dff158c246f65556eb65c
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-06-11 09:06:42 +0800

    define Add function from different package (calculation) but can be executable

---
+++ calculation/add.go
@@ -0,0 +1,6 @@
+package calculation
+
+//* define add function in calculation package
+func Add(firstNum int, secondNum int) int {
+	return firstNum + secondNum
+}
--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main //* must define 'main' package, because go app can be runned and builded
 
-import "fmt"
+import (
+	"golang-basic-fundamental/calculation" //*import different package 'module-name/package-name'
+	"fmt"
+)
 
 func main() {
 	fmt.Println("hello, i'm learning golang") //* for print data to console/terminal
 	sentence := introduction() //* define variables
 	fmt.Println(sentence)
+
+	addResult := calculation.Add(6, 8) //* calling imported different package 'package.Method'
+	fmt.Println(addResult)
 }
 
 //! Before develop an app with golang, make sure initialize app directory firstly with command 'go mod init 'name-app''
