fb872e3798b109862d7cd4c799ae9f172baac0dd
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-06-12 12:28:09 +0800

    append a new multiple key-value pair of map to existing slice

--- main.go
+++ main.go
@@ -11,6 +11,15 @@ func main() {
 
 	println("==================")
 
+		//* Insert a new map with multiple key-value pair in existing slice
+		citizen = append(citizen, map[string]string{
+			"name": "frans", "address": "pahlawan street 4",
+		})
+	
+		fmt.Println(citizen)
+	
+		println("===================")
+
 	for _, item := range(citizen) {
 		fmt.Println(item)
 	}
