1613bc7e9f9b50eb67476d68881d9ac29e37824e
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-06-18 09:42:58 +0800

    import and calling calculation function in main function

--- main.go
+++ main.go
@@ -3,10 +3,15 @@ package main
 import (
 	"fmt"
 	studentreport "golang-basic-fundamental/student-report"
+	calculation "golang-basic-fundamental/calculation"
 )
 
 func main() {
 	reportAhmad := studentreport.StudentReport(97)
 	
 	fmt.Println(reportAhmad)
+
+	calculationResult := calculation.Calculation(22, 4, "/")
+
+	fmt.Println(calculationResult)
 }
