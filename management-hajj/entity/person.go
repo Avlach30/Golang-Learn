10ed00b8ee932107a17174d3c91b4606e0eb9a75
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-06-19 13:45:41 +0800

    move ChangeHajjStatus method to 1 file with Person struct for easier organize

--- management-hajj/entity/person.go
+++ management-hajj/entity/person.go
@@ -4,3 +4,12 @@ type Person struct {
 	Name string
 	Age int
 }
+
+/* 
+	* Define a method for change Person name globally with pointer 
+	* accept parameter from struct
+	* parameter type must dereferencing for reassign value globally
+*/
+func (person *Person) ChangeHajjStatus() {
+	person.Name = "Hj. " + person.Name
+}
