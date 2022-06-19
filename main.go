654282251afa3b49f11ca9febb0b4870a0838a24
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-06-19 09:41:36 +0800

    define embedded or nested struct and describe it to function

--- main.go
+++ main.go
@@ -11,11 +11,36 @@ type Citizen struct {
 	IsMarried bool 
 }
 
-//* Define a function which is accept parameter from struct
+//* Define embedded/nested struct (One of struct data type is from another struct)
+type City struct {
+	Name string
+	Major Citizen //* Define data type is from another struct
+	Citizens []Citizen //* Define data type is array or slice of struct
+}
+
 func describeCitizen (citizen Citizen) string {
 	return fmt.Sprintf(
 		"Hello!, my name is %s, i live at %s, i'm %t married. You can contact me at %d", 
 		citizen.Name, citizen.Address, citizen.IsMarried, citizen.NumberPhone)
+}
+
+//* Define a function which is accept parameter from struct and returned string
+func describeCity (city City) string {
+	citizenTotal := len(city.Citizens)
+
+	describeCity := fmt.Sprintf(
+		"Welcome to %s city, this city is led by %s. Total population in this city is %d person",
+		city.Name, city.Major.Name, citizenTotal,
+	)
+
+	println("")
+
+	fmt.Println("Citizen list: ")
+	for _, citizen := range city.Citizens {
+		fmt.Println(citizen.Name)
+	}
+
+	return describeCity
 }
 
 func main() {
@@ -31,6 +56,8 @@ func main() {
 	describeAhmad := describeCitizen(ahmad) //* Calling a function with parameter of istance from struct
 	fmt.Println(describeAhmad)
 
+	println("============")
+
 	//* Make istance from struct with assign value undirectly
 	diana := Citizen{}
 	diana.Name = "Diana Anjani"
@@ -39,4 +66,21 @@ func main() {
 	diana.IsMarried = false
 
 	fmt.Println(diana)
+
+	println("============")
+
+	chandra := Citizen{
+		Name: "Chandra wijaya",
+		Address: "Pattimura 77",
+		NumberPhone: 628575438965,
+		IsMarried: true,
+	}
+
+	surabaya := City{
+		Name: "Surabaya",
+		Major: chandra,
+		Citizens: []Citizen{ahmad, diana, chandra}, //*Append or insert struct to array or slice
+	}
+
+	fmt.Println(describeCity(surabaya))
 }
