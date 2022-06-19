09ad41ff258607ce4276d07c59bac1a5e92cc9b3
Muhammad Zakia Avlach <19mzaki@gmail.com>
2022-06-19 12:28:08 +0800

    implement a pointer which is reference variable in go

---
+++ main.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	implementPointer(5)
+}
+
+func implementPointer(initNumber int) {
+	//* Implementing of pointer
+	secondNum := &initNumber 
+	//* Get a memory address where value of initNumber stored, this is called referencing
+	thirdNum := *secondNum 
+	//* Get a real value from pointer variable, this is called dereferencing
+
+	fmt.Println(secondNum)
+	fmt.Println(thirdNum)
+
+	*secondNum = initNumber * 3 
+	//* Reassign real value from pointer, so affected pointer source (initNumber) value is changed too
+	fmt.Println(*secondNum)
+
+	fmt.Println(initNumber)
+}
+
